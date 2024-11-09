a2a7445db484d470ece3eb27f7ae6f6fb6befbf1
Marc Addeo <marcaddeo@gmail.com>
2024-11-08 20:34:52 -0500

    Checkky point

--- src/lib.rs
+++ src/lib.rs
@@ -71,58 +71,104 @@ macro_rules! internal_record {
 
 #[macro_export]
 #[doc(hidden)]
+#[allow(clippy::crate_in_macro_def)]
 macro_rules! internal_new_record {
     // Done, generate struct and generate new_record associated function for model.
-    (@new_record () -> { $pub:vis $name:ident $(($field:ident : $type:ty))* }) => {
+    (@new_record () -> { $pub:vis $name:ident $(($field:ident : $type:ty))* } [ $(($option:ident : $option_type:ty))* ]) => {
         $crate::paste::paste! {
-            #[derive(Debug)]
+            // NewModelRecord
+            #[derive(Clone, Debug, Default, Insertable)]
+            #[diesel(table_name = crate::schema::[<$name:lower>])]
+            #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
             $pub struct [<New $name Record>]<'a> {
-                $($field : $type,)*
+                $($field : $type ,)*
+                $($option : $option_type ,)*
+            }
+
+            impl<'a> [<New $name Record>]<'a> {
+                // NewModelRecord::new
+                #[doc = "Create a new `" [<New $name Record>] "` object."]
+                pub fn new($($field : $type ,)*) -> [<New $name Record>]<'a> {
+                    Self {
+                        $($field ,)*
+                        $($option : None ,)*
+                    }
+                }
+
+            $(
+                // NewRecord::with_$option
+                #[doc = "Add the optional `" $option "` field to the `" [<New $name Record>] "` object."]
+                 pub fn [<with_ $option>](self, $option : $option_type) -> Self {
+                    Self {
+                        $option,
+                        ..self
+                    }
+                 }
+            )*
+
+                // NewModelRecord::create
+                #[doc = "Create a new `" [<$name:lower>] "` in the database."]
+                pub async fn create(&self, conn: &mut Connection) -> QueryResult<[<$name Record>]> {
+                    diesel::insert_into(crate::schema::[<$name:lower>]::table)
+                        .values(self)
+                        .returning(crate::schema::[<$name:lower>]::table::all_columns())
+                        .get_result(conn)
+                        .await
+                }
             }
 
             impl $name {
-                pub fn new_record<'a>($($field : $type),*) -> [<New $name Record>]<'a> {
-                    [<New $name Record>] {
-                        $($field,)*
-                    }
+                // Model::new_record
+                #[doc = "Create a new `" [<New $name Record>] "` object."]
+                pub fn new_record<'a>($($field : $type ,)*) -> [<New $name Record>]<'a> {
+                    [<New $name Record>]::new($($field ,)*)
                 }
             }
         }
     };
 
     // @TODO handle other owned types.
+    // @TODO seems like the Option<OwnedType> is going to be repetitive, maybe see if twe can get
+    // around that?
 
-    // Convert Option<String> fields to Option<&'a str>.
-    (@new_record ($field:ident : Option<String> $(, $($rest:tt)*)?) -> { $($output:tt)* }) => {
+    // Convert Option<String> fields to Option<&'a str>, and put them in the optionial accumulator.
+    (@new_record ($field:ident : Option<String> $(, $($rest:tt)*)?) -> { $($output:tt)* } [ $($optional:tt)* ]) => {
         $crate::defile::defile! {
-            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* ($field : Option<&'a str>) });
+            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* } [ $($optional)* ($field : Option<&'a str>) ]);
+        }
+    };
+
+    // Put optional fields in a separate optional accumulator.
+    (@new_record ($field:ident : Option<$type:ty> $(, $($rest:tt)*)?) -> { $($output:tt)* } [ $($optional:tt)* ]) => {
+        $crate::defile::defile! {
+            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* } [ $($optional)* ($field : Option<$type>) ]);
         }
     };
 
     // Convert String fields to &'a str.
-    (@new_record ($field:ident : String $(, $($rest:tt)*)?) -> { $($output:tt)* }) => {
+    (@new_record ($field:ident : String $(, $($rest:tt)*)?) -> { $($output:tt)* } [ $($optional:tt)* ]) => {
         $crate::defile::defile! {
-            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* ($field : &'a str) });
+            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* ($field : &'a str) } [ $($optional)* ]);
         }
     };
 
     // Remove id field.
-    (@new_record (id : $type:ty $(, $($rest:tt)*)?) -> { $($output:tt)* }) => {
+    (@new_record (id : $type:ty $(, $($rest:tt)*)?) -> { $($output:tt)* } [ $($optional:tt)* ]) => {
         $crate::defile::defile! {
-            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* });
+            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* } [ $($optional)* ]);
         }
     };
 
     // Iterate over struct fields.
-    (@new_record ($field:ident : $type:ty $(, $($rest:tt)*)?) -> { $($output:tt)* }) => {
+    (@new_record ($field:ident : $type:ty $(, $($rest:tt)*)?) -> { $($output:tt)* } [ $($optional:tt)* ]) => {
         $crate::defile::defile! {
-            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* ($field : $type) });
+            $crate::internal_new_record!(@@new_record ($($(@$rest)*)?) -> { $($output)* ($field : $type) } [ $($optional)* ]);
         }
     };
 
     // NewRecord entrypoint.
     ($pub:vis $name:ident ($($rest:tt)*)) => {
-        $crate::internal_new_record!(@new_record ($($rest)*) -> { $pub $name });
+        $crate::internal_new_record!(@new_record ($($rest)*) -> { $pub $name } []);
     };
 }
 
@@ -157,6 +203,8 @@ macro_rules! internal_model {
 #[allow(clippy::crate_in_macro_def)]
 macro_rules! internal_impl {
     // Done, generate model impl.
+    // @TODO i can probably change $name -> $model everywhere to make this a bit clearer. And get
+    // rid of the $model:ty here since i can just use the $model (name).
     (@impl () -> { $name:ident $(($field:ident : $type:ty))* } [ $(($key:ident ; $foreign_key:ident : $model:ty))* ] [ $(($many:ident : $many_model:ty))* ]) => {
         impl $name {
             $crate::paste::paste! {
@@ -168,6 +216,7 @@ macro_rules! internal_impl {
                             .await?;
                         let $key = $model::from_record(&$key, conn).await?;
                     )*
+
                     Ok($name {
                         $($key,)*
                         $(
@@ -176,6 +225,13 @@ macro_rules! internal_impl {
                         $($many : vec![],)*
                     })
                 }
+
+                // pub async fn from_records<'a>(
+                //     records: impl IntoIterator<Item = &'a PostRecord>,
+                //     conn: &'a mut Connection,
+                // ) -> Vec<Self> {
+                //     todo!()
+                // }
 
             $(
                 pub async fn [<with_ $many>](self, conn: &mut Connection) -> QueryResult<Self> {
